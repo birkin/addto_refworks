0d6564afb7e2e7eaa8c85a208ac182ad92163be3
birkin <birkin.diana@gmail.com>
2014-11-24 09:03:46 -0500

    refactors flow.

--- utils/josiah_to_refworks_v2.js
+++ utils/josiah_to_refworks_v2.js
@@ -8,6 +8,7 @@ console.log( "josiah_to_refworks_v2.js START" );
 
 var bibnum = null;
 var refworks_url = null;
+var image_holder = null;
 
 $(document).ready(
   function() {
@@ -34,55 +35,59 @@ function get_bibnum() {
 	if ( target_element != null ) {
 		var url = target_element.toString();
 		var start_position = url.lastIndexOf( "b" );
-		bibnum = url.substr( start_position, 8 );
-    build_refworks_url();
+		bibnum = url.substr( start_position, 8 );  // module var
+    if ( bibnum == null ) { return };  // possible end of processing
+    find_stub_image();
 	}
 	console.log( "bibnum, " + bibnum );
 }
 
-function build_refworks_url() {
-  /* Assembles and stores refworks url.
-   * Called by get_bibnum() when josiah page contains a bibnum.
-   */
-  var root_url = "http://www.refworks.com/express/ExpressImport.asp";
-  console.log( "refworks_url, " + refworks_url );
-  find_stub_image();
-}
-
 function find_stub_image() {
   /* Searches document's images to find the holder for the RefWorks image.
-   * Called by build_refworks_url()
+   * Called by get_bibnum() if a bibnum was found.
    */
-	if ( bibnum == null ) { return };
 	var images = document.images;
 	for (var i=0; i < images.length; i++) {
 		source_string = images[i].src;
 		var find_position = source_string.search( "screens/blank.gif" );
 		if ( find_position > 0 ) {
-      console.log( "match at, " + images[i].src );
-      add_refworks_image( images[i] )
+      image_holder = images[i];  // module var
+      console.log( "match at, " + image_holder.src );
+      add_refworks_image()
 		}
 	}
 }
 
-function add_refworks_image( image ) {
+function add_refworks_image() {
   /* Replaces stub image with 'Add to Refworks' image-button.
    * Called by find_stub_image()
    */
   var new_image = new Image();
   new_image.src = "/screens/btn_refworks.gif";
-  image.src=new_image.src;
-  image.alt="Add to RefWorks";
+  image_holder.src=new_image.src;
+  image_holder.alt="Add to RefWorks";
   console.log( "image added" );
-  // return;
-  update_click_url( image );
+  build_refworks_url();
+}
+
+function build_refworks_url() {
+  /* Assembles and stores refworks url.
+   * Called by add_refworks_image()
+   */
+  var root_url = "http://www.refworks.com/express/ExpressImport.asp";
+  refworks_url = "http://google.com";  // module var
+  console.log( "refworks_url, " + refworks_url );
+  update_click_url();
 }
 
 function update_click_url( image ) {
-  console.log( "image.parentElement, " + image.parentElement );
-  parent_a_tag = image.parentElement;
-  parent_a_tag.href = "http://google.com";
-  return;
+  /* Replaces pre-existing image-click url with built refworks url.
+   * Called by build_refworks_url()
+   */
+  console.log( "image.parentElement, " + image_holder.parentElement );
+  parent_a_tag = image_holder.parentElement;
+  parent_a_tag.href = refworks_url;
+  return;  // end of processing
 }
 
 
